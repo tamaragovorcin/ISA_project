76de031e39d9519777c6425c834f55b002cd97ee
Tamara Govorcin <tamara.govorcin@uns.ac.rs>
2021-02-03 12:50:13 +0100

    #fix patients subsribtions [Tamara]

---
+++ Backend/src/main/java/com/isaproject/isaproject/DTO/PharmacyIdDTO.java
@@ -0,0 +1,20 @@
+package com.isaproject.isaproject.DTO;
+
+public class PharmacyIdDTO {
+    private Integer pharmacyId;
+
+    public PharmacyIdDTO() {
+    }
+
+    public PharmacyIdDTO(Integer pharmacyId) {
+        this.pharmacyId = pharmacyId;
+    }
+
+    public Integer getPharmacyId() {
+        return pharmacyId;
+    }
+
+    public void setPharmacyId(Integer pharmacyId) {
+        this.pharmacyId = pharmacyId;
+    }
+}
